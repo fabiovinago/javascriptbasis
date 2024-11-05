3480a890b5976b62f9d4371ad775bafaf61d6ad5
fabiovinago <fabiovinago.be@gmail.com>
2024-11-05 09:36:22 +0100

    DOM

---
+++ DOM/index.js
@@ -0,0 +1,8 @@
+let introElement = document.getElementById("intro");
+console.log(introElement.textContent);
+
+let berichten = document.getElementsByClassName("bericht");
+console.log(berichten[1].textContent);
+
+let paragrafen = document.getElementsByTagName("p");
+console.log(paragrafen[0].textContent);
